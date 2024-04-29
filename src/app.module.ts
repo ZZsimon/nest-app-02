22fb1c9b25eb608f712aa16d66083780bc81fd2f
邵玉棋 <zz_simon@163.com>
2024-04-29 20:08:38 +0800

    finish

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PersonModule } from './person/person.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [PersonModule,
+   ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
